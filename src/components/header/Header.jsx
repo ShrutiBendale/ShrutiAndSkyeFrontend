09c03ddf0216c776e731374f873b83c85ec65802
Shruti Bendale <shrutibendale@gmail.com>
2023-01-23 00:16:54 -0800

    Add description to the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CoverImg from '../../assets/skye_nobg.png'
 import Socials from '../socials/Socials'
+import {AiOutlineDownCircle} from 'react-icons/ai';
 
 const Header = () => {
   return (
@@ -19,13 +20,27 @@ const Header = () => {
         <h2>Hi</h2>
       </div>       */}
 
-      <div className="header_right">
+      <div className="header_mid">
+        <div className='header_center_left'>
+          <h4>
+            I created this website to learn new technologies related to frontend and backend development. 
+            I will be using this page as a collection of links to all my projects and source code. 
+          </h4>
+          <h4>&nbsp;</h4>
+          <h4>   
+            So, follow along as I work on new projects and document my learnings!
+          </h4>
+          <h4>&nbsp;</h4>
+          <h4>But before you scroll down, meet my dog, Skye. She also happens to be the majority shareholder of this website.</h4>
+
+        </div>
         <img src= {CoverImg} alt="" />
+        
       </div>
 
     
       <div className='header_bottom'>
-        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        <a href="#contact"><AiOutlineDownCircle/></a>
       </div>
       <Socials/>
 
